026df497ce167a367b46ffe07f7c5bcb2fea67c2
jenshaak <jens.haakaas@gmail.com>
2024-11-29 16:53:47 +0100

    la til kommando for start

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Hello World!</h1>
+      <h1>Steps to get started:</h1>
+      <ol>
+        <li>npm i</li>
+        <li>rm -rf .git</li>
+        <li>npm run dev</li>
+      </ol>
     </main>
   );
 }
